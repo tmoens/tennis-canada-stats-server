6228944030beda424f3810ce9c22e53060da01b6
Taka Fumi Tsuchiya <taka@rscomconsulting.com>
2024-12-06 14:12:08 +0800

    Changes to stats admin usage report

--- src/modules/vrtournaments/license/license.service.ts
+++ src/modules/vrtournaments/license/license.service.ts
@@ -106,6 +106,7 @@ export class LicenseService {
       .addSelect('license.province', 'province')
       .addSelect('COUNT(tournament.tournamentCode)', 'tournamentCount')
       .addSelect('MAX(tournament.endDate)', 'mostRecent')
+      .addSelect('SUM(CASE WHEN YEAR(tournament.endDate) = 2024 THEN 1 ELSE 0 END)', 'tournamentCount2024')
       .leftJoin('license.tournaments', 'tournament')
       .where('license.licenseName IS NOT NULL')
       .groupBy('license.licenseName')
@@ -118,7 +119,7 @@ export class LicenseService {
     };
     wb.SheetNames.push('Licenses');
     const ws: WorkSheet = utils.json_to_sheet(licenses);
-    ws['!cols'] = [{ width: 50 }, { width: 13 }, { width: 11 }, { width: 14 }];
+    ws['!cols'] = [{ width: 50 }, { width: 13 }, { width: 11 }, { width: 14 },{ width: 14 }];
     wb.Sheets.Licenses = ws;
     const filename =
       'Reports/VR_License_Usage_' + now.toISOString().slice(0, 10) + '.xlsx';
